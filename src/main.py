44e8516b0b1482eb81a69fd9eb3346a6b0cd03cb
OTakumi <takumi.o.0410@gmail.com>
2023-05-28 17:24:30 +0900

    Added libraries for database connection, etc.

--- src/main.py
+++ src/main.py
@@ -1,9 +1,17 @@
 from typing import Union
 from fastapi import FastAPI
+import sqlalchemy
 
 import datetime
+from dotenv import load_dotenv
 
 app = FastAPI()
+load_dotenv()
+engine = sqlalchemy.create_engine("postgresql://" \
+        + 'POSTGRES_USER' \
+        + ":" \
+        + 'POSTGRES_PASSWORD' \
+        + "@localhost/db")
 
 @app.get("/")
 def read_root():
@@ -17,3 +25,9 @@ def read_item(item_id: int, q: Union[str, None] = None):
 def read_datetime_now():
     dt_now = datetime.datetime.now()
     return {"data": dt_now}
+
+@app.get("/info")
+def read_info():
+    sqlalchemy_version = sqlalchemy.__version__
+
+    return {"sqlalchemy": sqlalchemy_version}
